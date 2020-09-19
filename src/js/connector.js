645bfa5a1ed6c4c76559199c1301efef1db7c3df
葛新颖 <gxy23996@ly.com>
2020-09-19 11:55:30 +0800

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,8 +1,11 @@
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
-var onBtnClick = function (t, opts) {
-  console.log('Someone clicked the button');
+var onSortBtnClick = function (t, opts) {
+  console.log('Someone clicked the sort button');
 };
+var onDoneBtnClick = function (t, opts) {
+    console.log('Someone clicked the done button');
+  };
 
 window.TrelloPowerUp.initialize({
   'card-buttons': function (t, opts) {
@@ -10,16 +13,15 @@ window.TrelloPowerUp.initialize({
       // usually you will provide a callback function to be run on button click
       // we recommend that you use a popup on click generally
       icon: GRAY_ICON, // don't use a colored icon here
-      text: 'Open Popup',
+      text: '排序',
       callback: onBtnClick,
       condition: 'edit'
     }, {
       // but of course, you could also just kick off to a url if that's your thing
       icon: GRAY_ICON,
-      text: 'Just a URL',
-      condition: 'always',
-      url: 'https://developer.atlassian.com/cloud/trello',
-      target: 'Trello Developer Site' // optional target for above url
+      text: '完成',
+      callback: onDoneBtnClick,
+      condition: 'edit'
     }];
   }
 });
