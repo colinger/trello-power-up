002bfe5d043bf98664a384e8beba200fa45ba57c
葛新颖 <gxy23996@ly.com>
2020-09-19 12:57:37 +0800

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,7 +2,7 @@ var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4b
 
 var onSortBtnClick = function (t, opts) {
   console.log('Someone clicked the sort button');
-  console.log(JSON.stringify(opts));
+  console.log(JSON.stringify(t));
 };
 var onDoneBtnClick = function (t, opts) {
     console.log('Someone clicked the done button');
