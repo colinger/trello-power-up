6a66194c95721c563b8f31f574e741ac2243db5f
葛新颖 <gxy23996@ly.com>
2020-09-19 22:40:16 +0800

    update

--- src/js/connector.js
+++ src/js/connector.js
@@ -5,42 +5,40 @@ var onSortBtnClick = function (t, opts) {
   console.log(JSON.stringify(t));
 };
 var onDoneBtnClick = function (t, opts) {
-    t.getRestApi()
-        .getToken()
-        .then(function(token) {
-            if (!token) {
-                // do auth instead
-                console.log('error');
-            }
-            console.log(token);
-            // make a request with token
-    });
-    return t.card('all').then(function (card){
-        console.log(JSON.stringify(card, null, 2));
-        //card.id
+   return t.card('all').then(function (card){
+    console.log(JSON.stringify(card, null, 2));
+    //card.id
+    fetch('https://api.trello.com/1/cards/'+card.id+'?key=%%APP_KEY%%&token=%%TOKEN%%',{
+        method: 'PUT',
+        headers: {
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({"dueComplete":true})
+    })
+    .then(response => response.json())
+    .then(data => console.log(data));
    });
 };
 
 window.TrelloPowerUp.initialize({
   'card-buttons': function (t, opts) {
-    return t.getRestApi()
-    	// We now have an instance of the API client.
-      .isAuthorized()
-      .then(function(isAuthorized) {
-        if (isAuthorized) {
-          return [{
-            text: '排序',
-            callback: onSortBtnClick
-          }];
-        } else {
-          return [{
-            text: '完成',
-            callback: onDoneBtnClick
-          }];
-        }
-      });
+    return [{
+      // usually you will provide a callback function to be run on button click
+      // we recommend that you use a popup on click generally
+      icon: GRAY_ICON, // don't use a colored icon here
+      text: '排序',
+      callback: onSortBtnClick,
+      condition: 'edit'
+    }, {
+      // but of course, you could also just kick off to a url if that's your thing
+      icon: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/docomo/205/heavy-check-mark_2714.png',
+      text: '完成',
+      callback: onDoneBtnClick,
+      condition: 'edit'
+    }];
   }
-}, {
+}, 
+{
     appName: 'MyTool',
     appKey: '3223224f4dacfccedd728fac62c4352d'
 });
